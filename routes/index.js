47ca116461440822f50ba385bd915d495584ddd9
BUKASON <godfreyebuka4@gmail.com>
2021-02-08 04:58:46 -0800

    I created a file called together.jade in the views folder

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'EServices' });
+});
+
+/* GET together page. */
+router.get('/', function(req, res, next) {
+  res.render('together');
 });
 
 module.exports = router;
