75788c02e98fafb003175fae1e914e97f0546c51
BUKASON <godfreyebuka4@gmail.com>
2021-02-08 21:57:31 -0800

    I added Contact page, together page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,12 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'EServices' });
 });
 
+/* GET contact page. */
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
 /* GET together page. */
 router.get('/', function(req, res, next) {
   res.render('together');
