6df23a105bed4ee78ae2104c0a7ae5aab9793efb
Rufai Mustapha <rufai.mustopha@gmail.com>
2021-02-14 17:07:11 +0100

    completed drop-down for lga

--- public/javascripts/nigeria.js
+++ public/javascripts/nigeria.js
@@ -1,6 +1,31 @@
-console.log("This is sabinus")
 
-let stateListener = document.getElementById("states_in_nigeria")
-stateListener.addEventListener("click", (e) => {
-    console.log(stateListener.value)
+console.log("This is Cornel")
+
+let states_options = document.getElementById("states_in_nigeria")
+let list_of_lga = document.getElementById("list_lga")
+let list_of_lga_drop_down = document.getElementById("lga_in_state")
+
+states_options.addEventListener("click", (e) => {
+    // console.log(e.target.value)
+    let state_position = e.target.value
+    if (state_position) {
+        list_of_lga.innerHTML = ""
+        list_of_lga_drop_down.innerHTML = ""
+        let lgas_in_state = nigeria[state_position].state.locals
+        for ( index in lgas_in_state ) {
+            let lga = document.createElement("li")
+            let lga_option = document.createElement("option")
+            
+            lga.textContent = lgas_in_state[index].name
+            
+            lga_option.setAttribute("value", index)
+            lga_option.textContent = lgas_in_state[index].name
+
+
+            list_of_lga.appendChild(lga)
+            list_of_lga_drop_down.appendChild(lga_option)
+
+        }
+    }
 })
+
